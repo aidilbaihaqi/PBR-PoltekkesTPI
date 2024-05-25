c30581ff9b582ed4ae5a64a3da9ea6393135ea88
aidilbaihaqi <aidillmusirjun@gmail.com>
2024-05-25 21:15:06 +0700

    add eloquent relationship & tambah atribut stok di barang

--- app/Models/Ruang.php
+++ app/Models/Ruang.php
@@ -4,8 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Ruang extends Model
 {
-    use HasFactory;
+    protected $fillable = [
+        'nama_ruang',
+        'lokasi_ruang',
+        'kapasitas',
+        'status_ruang'
+    ];
+
+    public function peminjamanruang(): BelongsTo {
+        return $this->belongsTo(PeminjamanRuang::class);
+    }
 }
